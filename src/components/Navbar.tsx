d6668f79986ecaf2d19bd5807e1be8d8a16223f9
Emmanuel Tofa <emmanuelutofa@gmail.com>
2024-08-26 18:15:44 +0200

    Modify navbar code

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,22 +1,27 @@
 /** @jsxImportSource @emotion/react */
 import React from "react";
 import { css } from "@emotion/react";
-import ThemeToggleButton from "./toggleThemeButton"; // Ensure this path matches your project structure
-
-const navbarStyle = css`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 10px 20px;
-  color: #000;
-`;
-
-const titleStyle = css`
-  font-size: 18px;
-  font-weight: bold;
-`;
+import { useTheme } from "./themeContext";
+import ThemeToggleButton from "./toggleThemeButton";
 
 const Navbar: React.FC = () => {
+  const { theme } = useTheme();
+
+  const navbarStyle = css`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10px 20px;
+    background-color: ${theme === "light" ? "#f9f9f9" : "#333"};
+    color: ${theme === "light" ? "#000" : "#fff"};
+    transition: background-color 0.3s, color 0.3s;
+  `;
+
+  const titleStyle = css`
+    font-size: 18px;
+    font-weight: bold;
+  `;
+
   return (
     <nav css={navbarStyle}>
       <div css={titleStyle}>Song Management App</div>
